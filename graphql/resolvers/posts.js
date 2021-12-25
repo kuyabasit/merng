e5b1615340f7d827d55d2c4ff77785a77e43a78a
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-12-25 15:35:18 +0800

    create, delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,14 +1,65 @@
+const { AuthenticationError } = require('apollo-server');
+
 const Post = require('../../models/Post');
+const checkAuth = require('../../util/check-auth');
 
 module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
+        throw new Error(err);
+      }
+    },
+    async getPost(_, { postId }) {
+      try {
+        if (!postId.match(/^[0-9a-fA-F]{24}$/)) {
+          throw new Error('Invalid ID');
+        }
+        const post = await Post.findById(postId);
+        console.log(post);
+        if (post) {
+          return post;
+        } else {
+          throw new Error('Post not found');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = checkAuth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString(),
+      });
+
+      const post = await newPost.save();
+
+      return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (!post) {
+          return new Error('Post not found');
+        } else if (user.username === post.username) {
+          await post.delete();
+          return 'Post deleted successfully';
+        } else {
+          return new AuthenticationError('Action not allowed');
+        }
+      } catch (err) {
+        throw new Error(err);
       }
     },
   },
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -18,13 +18,16 @@ module.exports = gql`
     username: String!
     password: String!
     confirmPassword: String!
-    email: String
+    email: String!
   }
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
   }
 `;
