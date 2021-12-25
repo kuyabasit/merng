465f534847044b5992b28130fdf9325330e06629
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-12-25 17:03:43 +0800

    create, delete comment

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,11 +1,24 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
+  type Comment {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
+  }
   type Post {
     id: ID!
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+    likes: [Like]!
   }
   type User {
     id: ID!
@@ -29,5 +42,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
